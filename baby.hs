f34f81813db8d191e04e25b2b2ecfda70d5f7d62
Lorenz Gerber <lorenzottogerber@gmail.com>
2018-01-21 14:56:59 +0100

    almost there

--- baby.hs
+++ baby.hs
@@ -17,12 +17,36 @@ extractReg (a,_,(_,_)) = a
 extractTime :: ([Char], Bool, (Integer, Integer)) -> ([Char], Integer, Integer)
 extractTime (a,_,(b,c)) = (a, b, c)
 
+
+extractTime' :: ([Char], Bool, (Integer, Integer)) -> (Integer, Integer)
+extractTime' (_,_,(b,c)) = (b, c)
+
+sortByTime :: [([Char], Bool, (Integer, Integer))] -> [([Char], Bool, (Integer, Integer))]
+sortByTime a = sortBy (comparing extractTime) [(a,b,(c,d)) | (a,b,(c,d)) <- a]
+
 outgoingSorted :: [([Char], Bool, (Integer, Integer))] -> [([Char], Bool, (Integer, Integer))]
-outgoingSorted a = sortBy (comparing extractTime) [(a,b,(c,d)) | (a,b,(c,d)) <- a, b == False]
+outgoingSorted a = sortBy (comparing extractReg) [(a,b,(c,d)) | (a,b,(c,d)) <- a, b == False]
 
 
 incomingSorted :: [([Char], Bool, (Integer, Integer))] -> [([Char], Bool, (Integer, Integer))]
 incomingSorted a = sortBy (comparing extractReg) [(a,b,(c,d)) | (a,b,(c,d)) <- a, b == True]
+
+
+
+
+groupAndSort :: [([Char], Bool, (Integer, Integer))] -> [[([Char], Bool, (Integer, Integer))]]
+groupAndSort a = groupBy (\a b -> extractReg a == extractReg b) $ sort a 
+
+
+sortPhus :: [([Char], Bool, (Integer, Integer))] -> [([Char], Bool, (Integer, Integer))]
+sortPhus a = sortBy (comparing extractTime) a
+
+modTimePhus :: ([Char], Bool, (Integer, Integer)) -> ([Char], Integer)
+modTimePhus (a,True,(c,d)) = (a, hoursMinutesToMinutes (c,d))
+modTimePhus (a,False, (c,d)) = (a, (- hoursMinutesToMinutes (c,d)))
+ 
+groupedPhus :: [([Char], Bool, (Integer, Integer))] -> [[([Char], Integer)]]
+groupedPhus a = groupBy (\a b -> fst a == fst b) $ map modTimePhus $ sortPhus a
 
 doubleMe x = x + x
 doubleUs x y = doubleMe x + doubleMe y
