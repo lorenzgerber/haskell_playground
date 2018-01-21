13a4b42317ae67cb8f65b23cf37ba1fd0da9fb17
Lorenz Gerber <lorenz.gerber@lablicate.com>
2018-01-21 17:24:48 +0100

    solution works... but ugly us fog

--- baby.hs
+++ baby.hs
@@ -42,11 +42,31 @@ sortPhus :: [([Char], Bool, (Integer, Integer))] -> [([Char], Bool, (Integer, In
 sortPhus a = sortBy (comparing extractTime) a
 
 modTimePhus :: ([Char], Bool, (Integer, Integer)) -> ([Char], Integer)
-modTimePhus (a,True,(c,d)) = (a, hoursMinutesToMinutes (c,d))
-modTimePhus (a,False, (c,d)) = (a, (- hoursMinutesToMinutes (c,d)))
+modTimePhus (a,True,(c,d)) = (a, (- hoursMinutesToMinutes (c,d)))
+modTimePhus (a,False, (c,d)) = (a, hoursMinutesToMinutes (c,d))
  
 groupedPhus :: [([Char], Bool, (Integer, Integer))] -> [[([Char], Integer)]]
 groupedPhus a = groupBy (\a b -> fst a == fst b) $ map modTimePhus $ sortPhus a
+
+getFirstName :: [([Char], Bool, (Integer, Integer))] -> [[Char]] 
+getFirstName a = map fst $ map head $ groupedPhus a
+
+getTimes :: [([Char], Bool, (Integer, Integer))] -> [Integer]
+getTimes a = map (\ a -> sum $ map snd a ) $ groupedPhus a
+
+timeList :: [([Char], Bool, (Integer, Integer))] -> [([Char], Integer)]
+timeList a = zip (getFirstName a) (getTimes a)
+
+maxTimeReg :: [([Char], Bool, (Integer, Integer))] -> [Char]
+maxTimeReg a = fst $ maximumBy(comparing snd) (timeList a)
+
+timeListHHMM :: [([Char], Bool, (Integer, Integer))] -> [([Char], (Integer, Integer))]
+timeListHHMM a = zip (getFirstName a) (map minutesToHoursMinutes $ getTimes a)
+
+phus :: [([Char], Bool, (Integer, Integer))] -> ([Char], [([Char], (Integer, Integer))])
+phus a = ((maxTimeReg a), (timeListHHMM a))
+ 
+
 
 doubleMe x = x + x
 doubleUs x y = doubleMe x + doubleMe y
