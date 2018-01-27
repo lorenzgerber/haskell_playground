3adb92eb6e2f4a0a34fd68c07add80bcd2c008a1
Lorenz Gerber <lorenz.gerber@lablicate.com>
2018-01-27 16:18:06 +0100

    working on version 2

--- baby.hs
+++ baby.hs
@@ -1,123 +1,48 @@
-import Register
-import Data.List
-import Data.Ord
+module Shapes
+( Point(..)
+, Shape(..)
+, area
+, nudge
+, baseCircle
+, baseRect
+) where
 
-hoursMinutesToMinutes :: (Integer, Integer) -> Integer
-hoursMinutesToMinutes (h,m) = h * 60 + m
-
-minutesToHoursMinutes :: Integer -> (Integer, Integer)
-minutesToHoursMinutes m = (div m 60, mod m 60)
-
-extractIncoming :: ([Char], Bool, (Integer, Integer)) -> Bool
-extractIncoming (_,a,(_,_)) = a
-
-extractReg :: ([Char], Bool, (Integer, Integer)) -> [Char]
-extractReg (a,_,(_,_)) = a
-
-extractTime :: ([Char], Bool, (Integer, Integer)) -> ([Char], Integer, Integer)
-extractTime (a,_,(b,c)) = (a, b, c)
+data Point = Point Float Float deriving (Show)
 
 
-extractTime' :: ([Char], Bool, (Integer, Integer)) -> (Integer, Integer)
-extractTime' (_,_,(b,c)) = (b, c)
+data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
 
-sortByTime :: [([Char], Bool, (Integer, Integer))] -> [([Char], Bool, (Integer, Integer))]
-sortByTime a = sortBy (comparing extractTime) [(a,b,(c,d)) | (a,b,(c,d)) <- a]
+area :: Shape -> Float
+area (Circle _ r ) = pi * r ^ 2
+area (Rectangle (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - y1)
 
-outgoingSorted :: [([Char], Bool, (Integer, Integer))] -> [([Char], Bool, (Integer, Integer))]
-outgoingSorted a = sortBy (comparing extractReg) [(a,b,(c,d)) | (a,b,(c,d)) <- a, b == False]
+nudge :: Shape -> Float -> Float -> Shape
+nudge (Circle (Point x y) r) a b = Circle (Point (x+a) (y+b)) r
+nudge (Rectangle (Point x1 y1) (Point x2 y2)) a b = Rectangle (Point (x1+a) (y1+b)) (Point (x2+a) (y2+b))
 
+baseCircle :: Float -> Shape
+baseCircle r = Circle (Point 0 0) r
 
-incomingSorted :: [([Char], Bool, (Integer, Integer))] -> [([Char], Bool, (Integer, Integer))]
-incomingSorted a = sortBy (comparing extractReg) [(a,b,(c,d)) | (a,b,(c,d)) <- a, b == True]
+baseRect :: Float -> Float -> Shape
+baseRect width height = Rectangle (Point 0 0) (Point width height)
 
-
-
-
-groupAndSort :: [([Char], Bool, (Integer, Integer))] -> [[([Char], Bool, (Integer, Integer))]]
-groupAndSort a = groupBy (\a b -> extractReg a == extractReg b) $ sort a 
-
-
-sortPhus :: [([Char], Bool, (Integer, Integer))] -> [([Char], Bool, (Integer, Integer))]
-sortPhus a = sortBy (comparing extractTime) a
-
-modTimePhus :: ([Char], Bool, (Integer, Integer)) -> ([Char], Integer)
-modTimePhus (a,True,(c,d)) = (a, (- hoursMinutesToMinutes (c,d)))
-modTimePhus (a,False, (c,d)) = (a, hoursMinutesToMinutes (c,d))
+data Person = Person { firstName :: String
+                     , lastName :: String
+                     , age :: Int
+                     , height :: Float
+                     , phoneNumber :: String
+                     , flavor :: String } deriving (Show)
  
-groupedPhus :: [([Char], Bool, (Integer, Integer))] -> [[([Char], Integer)]]
-groupedPhus a = groupBy (\a b -> fst a == fst b) $ map modTimePhus $ sortPhus a
+data Vector a = Vector a a a deriving (Show)
 
-getFirstName :: [([Char], Bool, (Integer, Integer))] -> [[Char]] 
-getFirstName a = map fst $ map head $ groupedPhus a
+vplus :: (Num a) => Vector a -> Vector a -> Vector a
+(Vector i j k) `vplus` (Vector l m n) = Vector (i+l) (j+m) (k+n)
 
-getTimes :: [([Char], Bool, (Integer, Integer))] -> [Integer]
-getTimes a = map (\ a -> sum $ map snd a ) $ groupedPhus a
+dotProd :: (Num a) => Vector a -> Vector a -> a
+(Vector i j k) `dotProd` (Vector l m n) = i*l + j*m + k*n
 
-timeList :: [([Char], Bool, (Integer, Integer))] -> [([Char], Integer)]
-timeList a = zip (getFirstName a) (getTimes a)
-
-maxTimeReg :: [([Char], Bool, (Integer, Integer))] -> [Char]
-maxTimeReg a = fst $ maximumBy(comparing snd) (timeList a)
-
-timeListHHMM :: [([Char], Bool, (Integer, Integer))] -> [([Char], (Integer, Integer))]
-timeListHHMM a = zip (getFirstName a) (map minutesToHoursMinutes $ getTimes a)
-
-phus :: [([Char], Bool, (Integer, Integer))] -> ([Char], [([Char], (Integer, Integer))])
-phus a = ((maxTimeReg a), (timeListHHMM a))
- 
+vmult :: (Num a) => Vector a -> a -> Vector a
+(Vector i j k) `vmult` m = Vector (i*m) (j*m) (k*m)
 
 
-doubleMe x = x + x
-doubleUs x y = doubleMe x + doubleMe y
-
-maximum' :: (Ord a) => [a] -> a
-maximum' [] = error "maximum of empty list!"
-maximum' [x] = x
-maximum' (x:xs) = max x (maximum' xs) 
-
-replicate' :: Int -> a -> [a]
-replicate' n x
-    | n <= 0 = []
-    | otherwise = x : replicate' (n-1) x
-
-take' :: Int -> [a] -> [a]
-take' n _
-    | n <= 0 = []
-take' _ []   = []
-take' n ( x : xs ) = x : take' (n-1) xs 
-
-reverse' :: [a] -> [a]
-reverse' [] = []
-reverse' (x:xs) = reverse' xs ++ [x]
-
-zip' :: [a] -> [b] -> [(a,b)]
-zip' [] _ = []
-zip' _ [] = []
-zip' (x1:xs1) (x2:xs2) = (x1,x2) : zip' xs1 xs2
-
-elem' :: (Eq a) => a -> [a] -> Bool
-elem' _ [] = False
-elem' a (x:xs)
-    | a == x = True
-    | otherwise = elem' a xs
-
-qs :: (Ord a) => [a] -> [a]
-qs [] = []
-qs (x:xs) = qs [a | a <- xs, a <= x] ++ [x] ++ qs [a | a <- xs, a > x]
-
-qs' :: (Ord a) => [a] -> [a]
-qs' [] = []
-qs' (x:xs) =
-    let smallerOrEqual = [a | a <- xs, a <= x]
-        larger = [a | a <- xs, a > x]
-    in qs' smallerOrEqual ++ [x] ++ qs' larger
-
-
-
-multThree :: Int -> Int -> Int -> Int
-multThree x y z = x * y * z
-
-applyTwice :: (a -> a) -> a -> a
-applyTwice f x = f (f x)
 
--- phus2.hs
+++ phus2.hs
@@ -2,44 +2,27 @@ import Register
 import Data.List
 import Data.Ord
 
+--data Time a = Time (Integer, Integer) deriving (Show)
+type Time = (Integer, Integer)
 
-uniqueRegs :: [([Char], Bool, (Integer, Integer))] -> [[Char]]
-uniqueRegs x = nub $ map (\(a,b,(c,d)) -> a) x
+timeAdd :: Time -> Time -> Time 
+(a, b) `timeAdd` (c, d) = (a + c + div (b+d) 60, mod (b+d) 60)
+
+timeSub :: Time -> Time -> Time
+(a, b) `timeSub` (c, d) = (a - c + div (b-d) 60, mod (b - d) 60) 
+
+type PhusEntry = (String, Bool, Time)
+
+getTime :: PhusEntry -> Time
+getTime (_,_,a) = a
+
+isStart :: PhusEntry -> Bool
+isStart (_,a,_) = a
+
+isEnd :: PhusEntry -> Bool
+isEnd (_,a,_) = not a
+
+type PhusList = [PhusEntry]
 
 
-hoursMinutesToMinutes :: (Integer, Integer) -> Integer
-hoursMinutesToMinutes (h,m) = h * 60 + m
 
-minutesToHoursMinutes :: Integer -> (Integer, Integer)
-minutesToHoursMinutes m = (div m 60, mod m 60)
-
-extractTime :: ([Char], Bool, (Integer, Integer)) -> ([Char], Integer, Integer)
-extractTime (a,_,(b,c)) = (a, b, c)
-
-sortPhus :: [([Char], Bool, (Integer, Integer))] -> [([Char], Bool, (Integer, Integer))]
-sortPhus a = sortBy (comparing extractTime) a
-
-modTimePhus :: ([Char], Bool, (Integer, Integer)) -> ([Char], Integer)
-modTimePhus (a,True,(c,d)) = (a, (- hoursMinutesToMinutes (c,d)))
-modTimePhus (a,False, (c,d)) = (a, hoursMinutesToMinutes (c,d))
- 
-groupedPhus :: [([Char], Bool, (Integer, Integer))] -> [[([Char], Integer)]]
-groupedPhus a = groupBy (\a b -> fst a == fst b) $ map modTimePhus $ sortPhus a
-
-getFirstName :: [([Char], Bool, (Integer, Integer))] -> [[Char]] 
-getFirstName a = map fst $ map head $ groupedPhus a
-
-getTimes :: [([Char], Bool, (Integer, Integer))] -> [Integer]
-getTimes a = map (\ a -> sum $ map snd a ) $ groupedPhus a
-
-timeList :: [([Char], Bool, (Integer, Integer))] -> [([Char], Integer)]
-timeList a = zip (getFirstName a) (getTimes a)
-
-maxTimeReg :: [([Char], Bool, (Integer, Integer))] -> [Char]
-maxTimeReg a = fst $ maximumBy(comparing snd) (timeList a)
-
-timeListHHMM :: [([Char], Bool, (Integer, Integer))] -> [([Char], (Integer, Integer))]
-timeListHHMM a = zip (getFirstName a) (map minutesToHoursMinutes $ getTimes a)
-
-phus :: [([Char], Bool, (Integer, Integer))] -> ([Char], [([Char], (Integer, Integer))])
-phus a = ((maxTimeReg a), (timeListHHMM a))
